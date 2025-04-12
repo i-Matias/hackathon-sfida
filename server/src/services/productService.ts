0260298e8f7d0a2328b1055e8e49d2fbcfb339ff
Ina Bekteshi <inabekteshi26@gmail.com>
2025-04-12 14:16:33 +0200

    update routes

--- server/src/services/productService.ts
+++ server/src/services/productService.ts
@@ -1,12 +1,64 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const getAllProducts = async () => {
-  const products = await prisma.product.findMany();
+const createProduct = async (userId: number, data: any) => {
+  const product = await prisma.product.create({
+    data: {
+      name: data.name,
+      price: data.price,
+      quantity: data.quantity,
+      description: data.description,
+      userId: userId,
+    },
+  });
+
+  return product;
+};
+
+const getAllProducts = async (name?: string, category?: string) => {
+  const products = await prisma.product.findMany({
+    where: {
+      name: name ? { contains: name, mode: 'insensitive' } : undefined,  // Filter by name if provided
+    },
+    include: { user: { select: { username: true } } }, // Optional: Include user details if necessary
+  });
+
   return products;
 };
 
+const getProductsByUser = async (userId: number) => {
+  const products = await prisma.product.findMany({
+    where: { userId },
+  });
+
+  return products;
+};
+
+const updateProduct = async (productId: number, data: any) => {
+  const updatedProduct = await prisma.product.update({
+    where: { id: productId },
+    data: {
+      name: data.name,
+      price: data.price,
+      quantity: data.quantity,
+      description: data.description,
+    },
+  });
+
+  return updatedProduct;
+};
+
+const deleteProduct = async (productId: number) => {
+  await prisma.product.delete({
+    where: { id: productId },
+  });
+};
+
 export default {
+  createProduct,
   getAllProducts,
+  getProductsByUser,
+  updateProduct,
+  deleteProduct,
 };
